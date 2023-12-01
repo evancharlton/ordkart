79de9e4d061b21f5189a7ef96c7f6efffe34cb9f
Evan Charlton <evancharlton@microsoft.com>
2023-12-01 18:01:08 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd "$(git rev-parse --show-toplevel)"
+git checkout -B gh-pages
+(
+  cd app
+  npm ci
+  npm run build
+)
+git add -f app/build
+git commit -am "Rebuild website"
+git filter-branch -f --prune-empty --subdirectory-filter app/build
+git push -f origin gh-pages
+git checkout -
